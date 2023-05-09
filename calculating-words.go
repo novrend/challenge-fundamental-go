6d712b6a33bf84c54ada0514c5d7cb2bd6be0b66
ihza <ihzan0@gmail.com>
2023-05-09 20:13:28 +0700

    first commit

--- calculating-words.go
+++ calculating-words.go
@@ -4,7 +4,11 @@ import "fmt"
 
 func main() {
 	word := "selamat malam"
+	calculate := make(map[string]int)
 	for i := 0; i < len(word); i++ {
-		fmt.Println(string(word[i]))
+		letter := string(word[i])
+		calculate[letter]++
+		fmt.Println(letter)
 	}
+	fmt.Println(calculate)
 }
